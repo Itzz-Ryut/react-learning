19e8160ce1ce97b395f10119489bd277a4db3368
Aryan <aryan9933@gmail.com>
2026-02-27 00:39:53 +0530

    Done input event

--- src/topics/04-EventHandling/App.jsx
+++ src/topics/04-EventHandling/App.jsx
@@ -1,11 +1,20 @@
-function HandleClick() {
+//Click event
+/*function HandleClick() {
   console.log("ButtonClicked");
 }
 console.log("App rendering")
+*/
+
+//Input Events
+function HandleChange(event){
+  console.log(event.target.value)
+}
 function App() {
   return (
     <>
-      <button onClick={HandleClick}>Click</button>
+      {/* <button onClick={HandleClick}>Click</button> */}  
+      <input onChange={HandleChange} />
+      
     </>
   );
 }
