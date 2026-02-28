2d89b0bed4e9b0fde4a5356517864d167c8ebc6c
Aryan <aryan9933@gmail.com>
2026-02-28 11:28:10 +0530

    Done EventHandling: onSubmit

--- src/topics/04-EventHandling/App.jsx
+++ src/topics/04-EventHandling/App.jsx
@@ -1,21 +1,64 @@
-//Click event
-/*function HandleClick() {
-  console.log("ButtonClicked");
-}
-console.log("App rendering")
-*/
+import { useState } from "react";
 
-//Input Events
-function HandleChange(event){
-  console.log(event.target.value)
-}
+// ============================
+// Event Handling: onChange
+// ============================
+
+/*
 function App() {
+  const [text, setText] = useState("");
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+  const filteredList = [
+    "React",
+    "JavaScript",
+    "Python",
+    "TypeScript",
+    "Node",
+    "CSS",
+    "HTML",
+    "Vue",
+  ].filter((item) => item.toLowerCase().includes(text.toLowerCase()));
   return (
     <>
-      {/* <button onClick={HandleClick}>Click</button> */}  
-      <input onChange={HandleChange} />
-      
+      <input onChange={handleChange} placeholder='Search' />
+      {text !== "" ? filteredList.length == 0 ? (
+        <p>"No results found 😕"</p>
+      ) : (
+        <p>Searching For: {text}</p>
+      ) : null}
+
+      {filteredList.map((item) => (
+        <li key={item}> {item}</li>
+      ))}
     </>
   );
 }
+  */
+
+// ==========================
+// Event HAndling: onsubmit
+// ==========================
+
+function App() {
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Submitted:", name);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        onChange={(e) => setName(e.target.value)}
+        paaceholder='Enter name'
+      />
+      <button type='submit'>Submit</button>
+    </form>
+  );
+}
+
 export default App;
